ba18b22f0439f9207abdbff48ffca1d89e841b86
Saransh <singhsaransh494@gmail.com>
2022-07-21 08:32:38 +0530

    added thirsty prompt

--- hungry.py
+++ hungry.py
@@ -1,9 +1,14 @@
-hungry = input()
+hungry = input("Tell if you are hungry : ")
 
 if hungry == "yes":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
+    thirsty = input("Are you thirsty also : ")
+    if thirsty == "yes":
+        print("drink soda")
+    else:
+        print("eat and go outside")
 
 else:
     print("go and play outside")
